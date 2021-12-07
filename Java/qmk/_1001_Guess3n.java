a24876fe5ca2d261be2cb24fd3ebfb3c2dff0e3f
Miki_Kyoku <84458072+Miki-kyoku@users.noreply.github.com>
2021-12-07 12:04:23 +0800

    PAT_1001_害死人不偿命的3n+1猜想.java

--- Java/qmk/_1001_Guess3n.java
+++ Java/qmk/_1001_Guess3n.java
@@ -1,18 +1,25 @@
+/** 
+* Score: 15/15
+* Run Time: 80ms
+* Memory: 21900KB
+*/
+
+
 import java.util.Scanner;
 
 public class _1001_Guess3n{
     public static void main(String[] arg){
 
         Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        int counter =0;
+        int number = input.nextInt();             //输入一个正整数number
+        int counter =0;                           //计算总步数counter
 
-        while(number!=1){
+        while(number!=1){                         //若不为1，进入loop;换句话说，直到number=1,结束loop;
             if(number%2==0){
-                number=number/2;
+                number=number/2;                   //number为偶数，取其一半
             }
             else 
-                number = (3 * number + 1) / 2;
+                number = (3 * number + 1) / 2;       //反之，取其3N+1的一半
             counter++;
         }
 
