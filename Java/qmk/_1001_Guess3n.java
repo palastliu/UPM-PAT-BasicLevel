52fd11c4ac78ff4ca76971916e69555269d39b5e
Miki_Kyoku <84458072+Miki-kyoku@users.noreply.github.com>
2021-12-07 11:34:15 +0800

    Add files via upload

---
+++ Java/qmk/_1001_Guess3n.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class _1001_Guess3n{
+    public static void main(String[] arg){
+
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int counter =0;
+
+        while(number!=1){
+            if(number%2==0){
+                number=number/2;
+            }
+            else {
+                number = (3 * number + 1) / 2;
+            }
+            counter++;
+        }
+
+        System.out.println(counter);
+    }
+}
