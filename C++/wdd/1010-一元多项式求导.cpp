f6b507ca825d8b2b099db2b2920ac76d93b4963b
cs-buzz <67792865+cs-buzz@users.noreply.github.com>
2021-12-07 13:11:03 +0800

    Update 1010-一元多项式求导.cpp
    1010 一元多项式求导 (25 分)

--- C++/wdd/1010-一元多项式求导.cpp
+++ C++/wdd/1010-一元多项式求导.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int main10()
+int main()
 {
+    // 比如：a = 3; n = 4;
+    // 3 * x ^ 4; 一阶导数 = 12 x ^ 3
+    int n;
+    queue<int> nums;
+    while (cin >> n) {
+        nums.push(n);
+        if (cin.get() == '\n') {
+            break;
+        }
+    }
+
+    unsigned size = nums.size();
+    unsigned flag = 1;
+    while (!nums.empty()) {
+        int coefficient = nums.front();
+        nums.pop();
+        int index = nums.front();
+        nums.pop();
+
+        if (index != 0) {
+            if (size - 2 > nums.size()) {
+                cout << " ";
+            }
+            cout << coefficient * index << " " << index - 1;
+            flag = 0;
+        }
+    }
+
+    if (flag) {
+        cout << "0 0";
+    }
 
     return 0;
 }
